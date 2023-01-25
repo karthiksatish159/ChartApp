69d163c8dbff01e68d03f41e6d825d115ea56905
Karthik Satish Telagareddy <karthik.st@holosuit.com>
2023-01-25 16:37:50 +0530

    Socket io connection is estabilished

--- server/index.js
+++ server/index.js
@@ -2,6 +2,17 @@ const path=require('path');
 const publicPath=path.join(__dirname+"/../public");
 const express=require('express');
 const port=process.env.PORT||3000;
+const socketIO=require('socket.io');
 const app=express();
+const server=app.listen(port,()=>{console.log(`Server is running on ${port}`)})
+const io=socketIO(server);
+io.on('connection',(socket)=>
+{
+    console.log(`New user is connected ${socket.id}`);
+    socket.on('disconnect',()=>
+    {
+        console.log(`User was disconnected ${socket.id}`);
+    })
+})
 app.use(express.static(publicPath));
-app.listen(port,()=>{console.log(`Server is running on ${port}`)})
+
